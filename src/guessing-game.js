3d17c826305fc97a0469233476598b8d3092a27f
yessimov-a <a.yessimov98@gmail.com>
2019-10-09 23:37:56 +0600

    feat: complete guessing-game project

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._range = [];
+        this._index = 0;
+        this.l = 0;
+        this.r = 0;
+    }
 
     setRange(min, max) {
-
+        this.l = min;
+        this.r = max;
+        this._range = [min, max];
     }
 
     guess() {
-
+        this._index = Math.ceil((this.l + this.r) / 2);
+        return this._index;
     }
 
     lower() {
-
+        this.r = this._index;
     }
 
     greater() {
-
+        this.l = this._index;
     }
 }
 
 module.exports = GuessingGame;
+
